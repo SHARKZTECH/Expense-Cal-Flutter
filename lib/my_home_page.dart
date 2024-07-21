f0eeec4b2047dcd030f93739216ea7a8490f0480
sharkz <mosenjemudogoz@gmail.com>
2024-07-21 05:46:55 +0300

    Link to expense page

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,5 +1,6 @@
 import 'package:expense_calculator/create_expense_page.dart';
 import 'package:expense_calculator/expense.dart';
+import 'package:expense_calculator/expense_page.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -64,24 +65,30 @@ class ExpenseBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            const Icon(Icons.monetization_on),
-            Column(
-              children: [
-                Text("${item.category}: ${item.amount.toString()}"),
-                Text("spent on ${item.formattedDate}")
-              ],
-            ),
-            const Icon(Icons.arrow_forward),
-          ],
+    return GestureDetector(
+      child: Card(
+        margin: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Icon(Icons.monetization_on),
+              Column(
+                children: [
+                  Text("${item.category}: ${item.amount.toString()}"),
+                  Text("spent on ${item.formattedDate}")
+                ],
+              ),
+              const Icon(Icons.arrow_forward),
+            ],
+          ),
         ),
       ),
+      onTap: () {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const ExpensePage()));
+      },
     );
   }
 }
